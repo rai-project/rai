70b3c127d0c84c85edb3c5b061976d522dde75ad
Abdul Dakkak <abduld@wolfram.com>
2017-03-13 02:05:41 +0000

    add/fix version

--- version.go
+++ version.go
@@ -4,6 +4,7 @@ import "github.com/rai-project/cmd"
 
 var (
 	// These fields are populated by govvv
+	Version    string
 	BuildDate  string
 	GitCommit  string
 	GitBranch  string
@@ -13,6 +14,7 @@ var (
 
 func init() {
 	cmd.Version = cmd.VersionInfo{
+		Version:    Version,
 		BuildDate:  BuildDate,
 		GitCommit:  GitCommit,
 		GitBranch:  GitBranch,
