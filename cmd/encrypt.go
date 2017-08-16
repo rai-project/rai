4840c8b65807d3f03038fc564605a18b52589b97
Abdul Dakkak <abduld@wolfram.com>
2017-08-16 18:27:44 +0100

    add encrypt command

---
+++ cmd/encrypt.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+	"github.com/rai-project/utils"
+	"github.com/spf13/cobra"
+)
+
+// encryptCmd represents the encrypt command
+var encryptCmd = &cobra.Command{
+	Use:          "encrypt",
+	SilenceUsage: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return errors.Errorf("expected one argument, got %v", len(args))
+		}
+		val, err := utils.EncryptStringBase64(appSecret, args[0])
+		if err != nil {
+			return err
+		}
+		fmt.Println(val)
+		return nil
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(encryptCmd)
+}
