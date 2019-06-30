c4c729c85404b9975804c008ab9ac65e6bd3dbbf
aschuh703 <Blu3b3rry!>
2019-06-29 23:02:35 -0500

    Removed sanitize(wd) for Windows. Sanitize appears to change the working directory format to Linux and clean a linux working directory. But, Windows needs a Windows working directory.
    Windows executable now works.

--- cmd/utils.go
+++ cmd/utils.go
@@ -8,14 +8,13 @@ import (
 
 // Gets rid of volume drive label in Windows
 func sanitize(name string) string {
-	if len(name) > 1 && name[1] == ':' && runtime.GOOS == "windows" {
-		name = name[2:]
-	}
+	if runtime.GOOS == "linux" {
 
-	name = filepath.Clean(name)
-	name = filepath.ToSlash(name)
-	for strings.HasPrefix(name, "../") {
-		name = name[3:]
+		name = filepath.Clean(name)
+		name = filepath.ToSlash(name)
+		for strings.HasPrefix(name, "../") {
+			name = name[3:]
+		}
 	}
 
 	return name
